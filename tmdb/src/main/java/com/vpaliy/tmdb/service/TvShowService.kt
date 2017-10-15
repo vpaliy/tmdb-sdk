662dc446d955958bac82b3cba0f962317c038274
vpaliy <vpaliy97@gmail.com>
2017-10-15 12:58:45 -0400

    add TV services

--- tmdb/src/main/java/com/vpaliy/tmdb/service/TvShowService.kt
+++ tmdb/src/main/java/com/vpaliy/tmdb/service/TvShowService.kt
@@ -5,31 +5,31 @@ import com.vpaliy.tmdb.model.*
 import io.reactivex.Single
 
 class TvShowService(service:TvShows):TvShows by service{
-    fun details(id:String, options:QueryBuilder.()->QueryBuilder)
+    inline fun details(id:String, options:QueryBuilder.()->QueryBuilder)
         :Single<TvShowDetails>
         =details(id,options(QueryBuilder()).build())
 
-    fun accountStates(id:String,options:QueryBuilder.()->QueryBuilder)
+    inline fun accountStates(id:String,options:QueryBuilder.()->QueryBuilder)
         :Single<AccountState>
         =accountStates(id,options(QueryBuilder()).build())
 
-    fun changes(id:String,options:QueryBuilder.()->QueryBuilder)
+    inline fun changes(id:String,options:QueryBuilder.()->QueryBuilder)
             :Single<Changes>
             =changes(id,options(QueryBuilder()).build())
 
-    fun similar(id:String, options: QueryBuilder.() -> QueryBuilder)
+    inline fun similar(id:String, options: QueryBuilder.() -> QueryBuilder)
             :Single<ResultPage<TvShow>>
             =similar(id,options(QueryBuilder()).build())
 
-    fun recommendations(id:String, options: QueryBuilder.() -> QueryBuilder)
+    inline fun recommendations(id:String, options: QueryBuilder.() -> QueryBuilder)
             :Single<ResultPage<TvShow>>
             =recommendations(id,options(QueryBuilder()).build())
 
-    fun airingToday(options:QueryBuilder.()-> QueryBuilder)
+    inline fun airingToday(options:QueryBuilder.()-> QueryBuilder)
             :Single<ResultPage<TvShow>>
             =airingToday(options(QueryBuilder()).build())
 
-    fun tvOnAir(options:QueryBuilder.()-> QueryBuilder)
+    inline fun tvOnAir(options:QueryBuilder.()-> QueryBuilder)
             :Single<ResultPage<TvShow>>
             =tvOnAir(options(QueryBuilder()).build())
 }
