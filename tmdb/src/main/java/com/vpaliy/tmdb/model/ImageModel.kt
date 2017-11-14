8bcd79ff9b684f81cb7e6b49c2f38dddd440317c
vpaliy <vpaliy97@gmail.com>
2017-11-14 17:26:39 -0500

    fix ImageModel

--- tmdb/src/main/java/com/vpaliy/tmdb/model/ImageModel.kt
+++ tmdb/src/main/java/com/vpaliy/tmdb/model/ImageModel.kt
@@ -4,6 +4,6 @@ data class ImageModel(val aspect_ratio:Float,
                       val file_path:String,
                       val height:Int,
                       val iso_639_1:String?,
-                      val vote_average:Int,
+                      val vote_average:Double,
                       val vote_count:Int,
                       val width:Int)
--- tmdb/src/main/java/com/vpaliy/tmdb/model/MediaImages.kt
+++ tmdb/src/main/java/com/vpaliy/tmdb/model/MediaImages.kt
@@ -1,5 +1,3 @@
 package com.vpaliy.tmdb.model
 
-data class MediaImages(var id:Int,
-                       var backdrops:Array<ImageModel>?,
-                       var posters:Array<ImageModel>?)
+class MediaImages(var id:Int, var backdrops:Array<ImageModel>?, var posters:Array<ImageModel>?)
